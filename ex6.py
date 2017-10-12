94d0fd32442cbd35bfaefe5d127c03699a34d9bc
s184252 <s184252@ucs.ac.uk>
2017-10-12 12:03:13 +0100

    Initial commit using ternary operator

---
+++ ex6.py
@@ -0,0 +1,16 @@
+print("Enter your details when prompted!")
+name = input('Name: ')
+age = int(input('Age: '))
+height = int(input('Height(cm): '))
+weight = int(input('Weight(kg): '))
+eye_colour = input('Eye Colour: ')
+hair_colour = input('Hair Colour: ')
+
+print(f"Hello {name}! Let's discuss your details.")
+print(f"You are {age} years old at the moment", end='')
+
+# Trying ternary operator
+print(" grandad!") if(age > 30) else print(".")
+print(f"You are {height}cm tall, and weigh {weight}kg.", end='')
+print(".. I'd slow down on the chips if I were you.") if(weight > 40) print()
+print(f"Your eyes are {eye_colour} and your hair is {hair_colour}.")
