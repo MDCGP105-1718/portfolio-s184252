ef376e571d0231a5863f80b0bd3a8d897ef10644
s184252 <s184252@ucs.ac.uk>
2017-10-13 19:41:40 +0100

    Removed debug print statments

--- ex9.py
+++ ex9.py
@@ -7,17 +7,15 @@ house_cost = 1000000
 deposit_percentage = .25
 down_payment = house_cost * deposit_percentage
 num_months = 36
-
 current_savings = 0
-
 num_steps = 0
 low = 0
-high = 100000
+high = 10000
 
 # check to see if it is possible to save value at all to avoid binary search
 for month in range(num_months):
     current_savings += current_savings * annual_return / 12
-    current_savings += (current_salary / 12) * 1
+    current_savings += current_salary / 12
     if(month + 1 % 6 == 0):
         current_salary += current_salary * semi_annual_raise
 
@@ -25,17 +23,16 @@ if(current_savings < down_payment - 100):
     print("It is not possible to pay the down payment in three years")
     exit()
 elif(current_savings >= down_payment - 100 and current_savings <= down_payment + 100):
-    print(f"Percentage saved: {portion_saved * 100}\ncurrent_savings: {current_savings}")
+    print(f"Percentage saved: 100%\ncurrent_savings: {current_savings}")
+    print(f"Steps in bisection search: {num_steps}")
     exit()
 
 while(high > low):
     num_steps += 1
     current_salary = starting_salary
     current_savings = 0
-    print(f"low: {low} high: {high}")
     guess = int((high + low) / 2)
-    print(f"guess: {guess}")
-    portion_saved = guess / 100000
+    portion_saved = guess / 10000
 
     for month in range(num_months):
         current_savings += current_savings * annual_return / 12
@@ -44,8 +41,8 @@ while(high > low):
             current_salary += current_salary * semi_annual_raise
 
     if(current_savings >= down_payment - 100 and current_savings <= down_payment + 100):
-        print(f"Percentage saved: {portion_saved * 100}\ncurrent_savings: {current_savings}")
-        print(f"num_steps: {num_steps}")
+        print(f"Percentage saved: {portion_saved * 10}%\ncurrent_savings: {current_savings}")
+        print(f"Steps in bisection search: {num_steps}")
         exit()
     elif(current_savings < down_payment - 100):
         low = guess
