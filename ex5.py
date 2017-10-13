46d58b7ded5d08ff5509defcbf110b27383b607c
s184252 <s184252@ucs.ac.uk>
2017-10-13 19:10:03 +0100

    cleaned up print statements

--- ex5.py
+++ ex5.py
@@ -1,12 +1,12 @@
 num_bottles = 99
 
 while(num_bottles > 2):
-	print(f"{num_bottles} bottles of beer on the wall, {num_bottles} bottles of beer.\n" +
-		  f"Take one down, pass it around, {num_bottles - 1} bottles of beer on the wall...\n")
+	print(f"{num_bottles} bottles of beer on the wall, {num_bottles} bottles of beer.")
+	print(f"Take one down, pass it around, {num_bottles - 1} bottles of beer on the wall...\n")
 	num_bottles -= 1
 
-print("2 bottles of beer on the wall, 2 bottles of beer.\n" +
-	  "Take one down, pass it around, 1 bottle of beer on the wall...\n")
+print("2 bottles of beer on the wall, 2 bottles of beer.")
+print("Take one down, pass it around, 1 bottle of beer on the wall...\n")
 
-print("1 bottle of beer on the wall, 1 bottle of beer!\n" +
-	  "So take it down, pass it around, no more bottles of beer on the wall!\n")
+print("1 bottle of beer on the wall, 1 bottle of beer!")
+print("So take it down, pass it around, no more bottles of beer on the wall!\n")
