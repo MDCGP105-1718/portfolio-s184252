83ef1b8cfa48d6578b1318ffdf237e5cbe865b3d
s184252 <s184252@ucs.ac.uk>
2017-11-03 12:54:42 +0000

    Finished most frequent words function.

--- week_6/task_2.py
+++ week_6/task_2.py
@@ -1,24 +1,34 @@
+# Hardcoded lyrics filenames
 LYRICS1_FILENAME = "lyrics1.txt"
 LYRICS2_FILENAME = "lyrics2.txt"
 
-def find_most_frequent_words(lyrics):
+def find_most_frequent_words(usr_string):
 
-	lyrics_no_punc = []
+	usr_string_no_punc = []
 	words_dict = {}
 
-	for c in lyrics.lower():
+	for c in usr_string.lower():
 		if c in "abcdefghijklmnopqrstuvwxyz' \n":
-			lyrics_no_punc.append(c)
+			usr_string_no_punc.append(c)
 
-	lyrics_no_punc = "".join(lyrics_no_punc)
+	usr_string_no_punc = "".join(usr_string_no_punc)
 
-	for word in lyrics_no_punc.split():
+	for word in usr_string_no_punc.split():
 		if word not in words_dict:
-			words_dict[words] = 1
+			words_dict[word] = 1
 		else:
 			words_dict[word] += 1
+
+	max_freq = max(words_dict.values())
+	most_freq_words = []
+
+	for key, value in words_dict.items():
+		if value == max_freq:
+			most_freq_words.append(key)
+
+	return (most_freq_words, max_freq)
 
 
 lyrics1 = open(LYRICS1_FILENAME, 'r').read()
 
-find_most_frequent_words(lyrics1)
+print(find_most_frequent_words(lyrics1))
