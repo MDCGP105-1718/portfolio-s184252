65c1f9efcff933f181c18ca3f4d5ae565cedf871
Kenny Pirkle <s184252@ucs.ac.uk>
2017-11-25 14:08:54 +0000

    Initial implementation of deck, hand and card classes.

---
+++ week_9/task_1.py
@@ -0,0 +1,70 @@
+from random import randint
+
+class Hand(object):
+    def __init__(self, cards):
+        self.cards = cards
+
+    def __str__(self):
+        hand_string = []
+        for card in self.cards:
+            hand_string.append(str(card))
+        return "\n".join(hand_string)
+
+class Card(object):
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
+        self.index = randint(0, 51)
+
+    def __str__(self):
+        return self.value + " of " + self.suit
+
+class Deck(object):
+    def __init__(self):
+        card_suits = [
+            'Spades',
+            'Hearts',
+            'Clubs',
+            'Diamonds'
+        ]
+
+        card_values = [
+            'Ace',
+            'Two',
+            'Three',
+            'Four',
+            'Five',
+            'Six',
+            'Seven',
+            'Eight',
+            'Nine',
+            'Ten',
+            'Jack',
+            'Queen',
+            'King'
+        ]
+
+        self.cards = []
+        for n in range(0, 52):
+            self.cards.append(Card(card_suits[n // 13], card_values[n % 13]))
+
+    def __str__(self):
+        deck_string = []
+        for card in self.cards:
+            deck_string.append(str(card))
+        return "\n".join(deck_string)
+
+    def shuffle(self):
+        self.cards.sort(key = lambda x : x.index)
+
+    def deal_hand(self, num_cards = 5):
+        hand = []
+        for n in range(0, num_cards):
+            hand.append(self.cards.pop())
+        return Hand(hand)
+
+
+deck = Deck()
+deck.shuffle()
+hand = deck.deal_hand(num_cards = 7)
+print(hand)
